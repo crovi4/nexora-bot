933ba8b06bea44a492657b3ac683991ff7dc21ba
Crovi4 <crovi4@tuta.io>
2026-01-05 05:43:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
+import discord
+from discord.ext import commands
+import os
 
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ {bot.user} запущен")
+    await bot.tree.sync()
+
+async def load_cogs():
+    for file in os.listdir("./cogs"):
+        if file.endswith(".py"):
+            await bot.load_extension(f"cogs.{file[:-3]}")
+
+async def main():
+    async with bot:
+        await load_cogs()
+        from config import TOKEN
+        await bot.start(TOKEN)
+
+import asyncio
+asyncio.run(main())
